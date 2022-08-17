9606676c0a10c9f83eb0a9fdf3b94b66a2a0801b
J4cKaLLL <j4ckal.r@gmail.com>
2022-08-17 17:41:32 -0500

    make orders

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -8,9 +8,42 @@ contract Exchange {
 	address public feeAccount;
 	uint256 public feePercent;
 	mapping(address => mapping (address => uint256)) public tokens;
+	mapping(uint256 => _Order) public orders;
+	uint256 public orderCount; 
 
-	event Deposit(address token, address user, uint256 amount, uint balance);
-	event Withdraw(address token, address user, uint256 amount, uint balance);
+	event Deposit(
+		address token, 
+		address user, 
+		uint256 amount, 
+		uint balance);
+	event Withdraw(
+		address token, 
+		address user, 
+		uint256 amount, 
+		uint balance);
+
+	event Order(
+			// Attributes of an order
+		uint256 id,        // Unique identifier for order
+		address user,     // User who made the order
+		address tokenGet,  // Address of the token they receive
+		uint256 amountGet, // Amount they receive
+		address tokenGive, // Address of token they give
+		uint256 amountGive, // Amount they give
+		uint256 timestamp);
+		
+
+	// Orders Mapping
+	struct _Order {
+		// Attributes of an order
+		uint256 id;        // Unique identifier for order
+		address user;      // User who made the order
+		address tokenGet;  // Address of the token they receive
+		uint256 amountGet; // Amount they receive
+		address tokenGive; // Address of token they give
+		uint256 amountGive; // Amount they give
+		uint256 timestamp;
+	}
 
 	constructor(address _feeAccount, uint256 _feePercent){
 		feeAccount = _feeAccount;
@@ -50,5 +83,37 @@ contract Exchange {
 		
 		return tokens[_token][_user];
 	}
+
+	function makeOrder(
+		address _tokenGet, 
+		uint256 _amountGet, 
+		address _tokenGive, 
+		uint256 _amountGive
+	) public{
+		require(balanceOf(_tokenGive, msg.sender) >= _amountGive);
+
+		// CREATE ORDER
+		orderCount = orderCount + 1;
+		orders[orderCount] = _Order(
+			orderCount, 	
+			msg.sender, 	
+			_tokenGet,  	
+			_amountGet,		
+			_tokenGive,		
+			_amountGive,	
+			block.timestamp 
+		);
+		emit Order(
+			orderCount, 	
+			msg.sender, 	
+			_tokenGet,  	
+			_amountGet,		
+			_tokenGive,		
+			_amountGive,	
+			block.timestamp 
+		);
+	}
+
 }
+
 
