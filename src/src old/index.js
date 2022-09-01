11fe94a849c5a1c8fff81941c9e9fe58a74bdf30
J4cKaLLL <j4ckal.r@gmail.com>
2022-09-01 17:11:27 -0500

    18. Navbar

--- src/src old/index.js
+++ src/src old/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './App.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
@@ -9,7 +8,7 @@ import store from './store/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+  <Provider store = {store}>
     <App />
   </Provider>
 );
